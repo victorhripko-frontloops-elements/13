33e75ac13bd765de34feacbbaf79fb265b39d536
Victor Hripko <hripko.v@soft2bet.com>
2020-09-03 14:10:01 +0300

    ref

--- src/index.js
+++ src/index.js
@@ -10,44 +10,34 @@ import './style.scss';
   const offset = 15;
 
   let currentSlide = 0;
-  let canSlide = true;
+  let lastSlide = false;
 
   const maxTranslate = [...cards].reduce(
     (total, current) => total + current.offsetWidth + offset,
     -slider.offsetWidth + offset
   );
 
-  const slideCard = (slide) => {
+  const goToSlide = (slideIndex) => {
+    const slide = cards[slideIndex];
     const translateValue = -slide.offsetLeft + offset;
-    const need = translateValue < -maxTranslate ? -maxTranslate : translateValue;
+    const translateToLast = translateValue < -maxTranslate;
+    const need = translateToLast ? -maxTranslate : translateValue;
 
-    canSlide = need === translateValue;
+    if (!slide) return;
+
+    lastSlide = translateToLast;
+    currentSlide = slideIndex;
 
     slider.style = `transform: translate3d(${need}px, 0, 0)`;
     updateButtons();
   };
 
-  prevBtn.addEventListener('mousedown', () => {
-    const nextSlide = cards[currentSlide - 1];
-    if (!nextSlide) return;
-
-    currentSlide--;
-    slideCard(nextSlide);
-  });
-
-  nextBtn.addEventListener('mousedown', () => {
-    const nextSlide = cards[currentSlide + 1];
-    if (!nextSlide) return;
-
-    if (canSlide) {
-      currentSlide++;
-      slideCard(nextSlide);
-    }
-  });
+  prevBtn.addEventListener('mousedown', () => goToSlide(currentSlide - 1));
+  nextBtn.addEventListener('mousedown', () => goToSlide(currentSlide + 1));
 
   const updateButtons = () => {
     prevBtn.classList.toggle('is-hidden', currentSlide <= 0);
-    nextBtn.classList.toggle('is-hidden', !canSlide);
+    nextBtn.classList.toggle('is-hidden', lastSlide);
   };
 
   updateButtons();
